f3fab568fa75db452c4e17c761359fadb60467d6
Ramjivan <ramjeewanj@gmail.com>
2020-06-12 21:27:16 +0530

    default nginx update

--- cloud-config.bash
+++ cloud-config.bash
@@ -1,4 +1,2 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/Ramjivan/sx_container_init/master/container_name
-var=`cat container_name`
-docker run -d -p 80:80 --name nginx "$var"
+echo "{"hostname":"user.jiva.streamx.app","uid":"asmndf","email":"dsjfn@kfd.com","streamKey":"streamKey","apiKey":"apiKey"}" > metadata.json;echo "{"status":"stopped"}" > state.json
---
+++ test.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+ls;ls;cat state.json
