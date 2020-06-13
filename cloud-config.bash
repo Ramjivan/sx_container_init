3fc7677d7119a737a1d78688cb0e3db645f19169
Ramjivan <ramjeewanj@gmail.com>
2020-06-13 14:17:07 +0530

    nginx_conf update to fix api redirect

--- cloud-config.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "{"hostname":"user.jiva.streamx.app","uid":"asmndf","email":"dsjfn@kfd.com","streamKey":"streamKey","apiKey":"apiKey"}" > metadata.json;echo "{"status":"stopped"}" > state.json
